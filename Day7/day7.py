cdd41d540d60535193575c5a61b1101d7aa4f570
+ <+>
2023-05-21 16:17:53 +0300

    day 7 completed

--- Day7/day7.py
+++ Day7/day7.py
@@ -10,3 +10,13 @@ target_sum = 15
 combinations = find_combinations(number_list, target_sum)
 for combination in combinations:
     print(combination)
+
+
+
+#program to return a new set with unique items from both sets by removing duplicates
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+unique_set = set1 | set2
+
+print(unique_set)
